35313ddc1402092e689ec74c493193590b674b0c
esrau <77896345+esrau@users.noreply.github.com>
2021-09-09 16:03:34 +0300

    Add files via upload

--- WeeklySalaryCalculator.java
+++ WeeklySalaryCalculator.java
@@ -3,7 +3,7 @@ package week1;
 import java.util.Scanner;
 
 public class WeeklySalaryCalculator {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Please enter base salary:");
         Scanner scanner = new Scanner(System.in);
         double baseSalary = scanner.nextInt();
@@ -16,42 +16,41 @@ public class WeeklySalaryCalculator {
 
         if (baseSalary < 1000) {
             System.out.println("Base salary must be grater than 1000");
-            System.out.println("Please enter base salary:");
-            baseSalary = scanner.nextInt();
         }
-        double hourlySalary = (baseSalary/40);
-
+        double hourlySalary = (baseSalary / 40);
         System.out.println("Please enter weekday working hour:");
         int weekdayWorkingHour = scanner.nextInt();
-
         System.out.println("Please enter weekend working hour:");
         int weekendWorkingHour = scanner.nextInt();
         scanner.close();
 
         int totalWorkingHour = (weekdayWorkingHour + weekendWorkingHour);
 
-        if((weekdayWorkingHour < 40) && (totalWorkingHour > 40)) {
+        if ((weekdayWorkingHour < 40) && (totalWorkingHour > 40)) {
             weekendWork = totalWorkingHour - 40;
             weekendSalaryAddition = (weekendWork * hourlySalary) * 2;
         }
-        else if ((weekdayWorkingHour > 40) && (weekendWorkingHour >= 0)) {
+        if ((weekdayWorkingHour >= 40) && (weekendWorkingHour >= 0)) {
             weekdaySalaryAddition = ((weekdayWorkingHour - 40) * hourlySalary) * 1.5;
             weekendSalaryAddition = (weekendWorkingHour * hourlySalary) * 2;
         }
-        if(totalWorkingHour < 40){
+        if (totalWorkingHour < 40) {
             salaryDeduction = (40 - totalWorkingHour) * hourlySalary;
         }
-        if((weekdayWorkingHour >= 40) && (weekendWorkingHour > 10)) {
-            weeklySalary = (baseSalary + weekdaySalaryAddition + weekendSalaryAddition + bonus);
-            System.out.println("Weekly salary is : " + weeklySalary);
+        if (totalWorkingHour >= 40) {
+            if(weekendWorkingHour >= 10){
+                weeklySalary = (baseSalary + weekdaySalaryAddition + weekendSalaryAddition + bonus);
+                System.out.println("Weekly salary is : " + weeklySalary);
+            }
+            else{
+                weeklySalary = (baseSalary + weekdaySalaryAddition + weekendSalaryAddition);
+                System.out.println("Weekly salary is : " + weeklySalary);
+            }
         }
-        if((totalWorkingHour > 40) && (weekendWorkingHour < 10)) {
-            weeklySalary = (baseSalary + weekdaySalaryAddition + weekendSalaryAddition);
-            System.out.println("Weekly salary is : " + weeklySalary);
-        }
-        if(totalWorkingHour < 40) {
+        else{
             weeklySalary = (baseSalary - salaryDeduction);
             System.out.println("Weekly salary is : " + weeklySalary);
         }
+        }
     }
-}
+
